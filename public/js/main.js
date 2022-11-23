f5e29da5bfc4f786b7ad358d31032fc20d1b2d2a
James Hwang <jaehwa.jh@gmail.com>
2022-11-23 01:59:15 -0800

    added new add button

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,31 @@
-// Drop down menu
+// Popup Window
+
+let modal = document.getElementById("modal");
+function modalHandler(val) {
+    if (val) {
+        fadeIn(modal);
+    } else {
+        fadeOut(modal);
+    }
+}
+function fadeOut(el) {
+    el.style.opacity = 1;
+    (function fade() {
+        if ((el.style.opacity -= 0.1) < 0) {
+            el.style.display = "none";
+        } else {
+            requestAnimationFrame(fade);
+        }
+    })();
+}
+function fadeIn(el, display) {
+    el.style.opacity = 0;
+    el.style.display = display || "flex";
+    (function fade() {
+        let val = parseFloat(el.style.opacity);
+        if (!((val += 0.2) > 1)) {
+            el.style.opacity = val;
+            requestAnimationFrame(fade);
+        }
+    })();
+}
