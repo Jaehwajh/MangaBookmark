3b75e8ab579a9e2a46348da740501e1ce2afdb0c
James Hwang <jaehwa.jh@gmail.com>
2022-11-30 00:09:54 -0800

    redid dashboard functions

--- models/Bookmarks.js
+++ models/Bookmarks.js
@@ -6,31 +6,14 @@ const BookmarkSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    // Image Cover
-    // Synopsis
     url: {
         type: String,
         required: true
     },
-    // caption: {
-    //     type: String,
-    //     required: true
-    // },
-    // rating: {
-    //     type: Number 
-    // },
     completed: {
         type: Boolean,
         required: true
     },
-    // reading: {
-    //     type: Boolean,
-    //     required: true
-    // },
-    // readlist: {
-    //     type: Boolean,
-    //     required: true
-    // },
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User"
