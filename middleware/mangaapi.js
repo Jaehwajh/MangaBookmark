904a46d1985060aa1e68b62780b5b1f3091cd3d6
James Hwang <jaehwa.jh@gmail.com>
2022-12-06 17:59:17 -0800

    added manga api scrapper support

---
+++ middleware/mangaapi.js
@@ -0,0 +1,16 @@
+const axios = require("axios");
+
+const options = {
+  method: 'GET',
+  url: 'https://manga-scrapper.p.rapidapi.com/search//',
+  headers: {
+    'X-RapidAPI-Key': process.env.KEY,
+    'X-RapidAPI-Host': process.env.HOST
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
