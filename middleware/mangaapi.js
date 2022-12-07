fa101ea782ef3b40ec62ed641f8e74b7305b6ea6
James Hwang <jaehwa.jh@gmail.com>
2022-12-07 01:14:01 -0800

    edited dashboard

--- middleware/mangaapi.js
+++ middleware/mangaapi.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 const options = {
   method: 'GET',
-  url: 'https://manga-scrapper.p.rapidapi.com/search//',
+  url: `https://manga-scrapper.p.rapidapi.com/search/${searchMangas}/`,
   headers: {
     'X-RapidAPI-Key': process.env.KEY,
     'X-RapidAPI-Host': process.env.HOST
