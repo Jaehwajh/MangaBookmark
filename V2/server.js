20bf8fc359f2e355799e483d6ffb96729d47390b
Jaehwa <Jaehwa.jh@gmail.com>
2024-01-05 18:01:46 -0800

    server.js

--- V2/server.js
+++ V2/server.js
@@ -7,3 +7,38 @@ const methodOverride = require("method-override");
 const flash = require("express-flash");
 const logger = require("morgan");
 
+// Dot env
+require("dotenv").config();
+
+// App
+app.use(express.static(__dirname + "/V2/pubic"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(logger("dev"));
+app.use(methodOverride("_method"));
+
+// MongoDB
+const database = process.env.URL;
+
+const connectDB = async() => {
+    try{
+        await mongoose.connect(database, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+        });
+        console.log(`Database Connected`);
+    }catch(err){
+        console.log(err);
+        process.exit(1);
+    }
+};
+
+connectDB();
+
+app.listen(process.env.PORT, () =>{
+    console.log("Test Build Online")
+});
+
+
